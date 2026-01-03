9246f1c6a1a271cec9db19e29d8351c043021f45
prakharsh95 <prakhar.sh95@gmail.com>
2026-01-03 12:12:18 +0530

    Adding delete order method

--- utils/API_utils.py
+++ utils/API_utils.py
@@ -1,15 +1,45 @@
+
+
 class API_Utils():
     def __init__(self, playwright):
         self.playwright = playwright
         self.context = self.playwright.request.new_context(base_url="https://rahulshettyacademy.com")
+        self.token = None
+        self.user_id = None
 
     def login(self):
         response = self.context.post(url='/api/ecom/auth/login',
                                      headers={'Content-Type': 'application/json'},
                                      data={"userEmail": "prakhar.sh95@gmail.com", "userPassword": "Password@123"})
         response_json = response.json()
+        
+        self.token = response_json['token']
+        self.user_id = response_json['userId']
+        
+        return self.token, self.user_id
 
-        return response_json['token'], response_json['userId']
+    def create_order(self):
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': self.token}
+        payload = {"orders":[{"country":"India",
+                              "productOrderedId":"68a961459320a140fe1ca57a"}]}
+
+        response = self.context.post(url='/api/ecom/order/create-order',
+                                     data=payload,
+                                     headers=headers)
+        response_json = response.json()
+        assert response_json['message']
+        return response_json['orders'][0]
+
+    def delete_order(self, order_id):
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': self.token}
+        response = self.context.delete(url=f'/api/ecom/order/delete-order/{order_id}',
+                                       headers=headers)
+        assert response.status == 200, f"Expected status code 200, but got {response.status}"
+        response_json = response.json()
+        assert response_json['message'] == "Orders Deleted Successfully", f"Expected message 'Orders Deleted Successfully', but got '{response_json.get('message', 'No message field')}'"
+
 
 
 

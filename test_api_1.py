14746862cfc239c094310eb61784be9fb2a1b2ad
prakharsh95 <prakhar.sh95@gmail.com>
2026-01-03 12:33:07 +0530

    Writing basic test case for UI

--- test_api_1.py
+++ test_api_1.py
@@ -7,3 +7,19 @@ def test_api1(playwright:Playwright):
                  headers={'Content-Type':'application/json'},
                  data={"userEmail": "prakhar.sh95@gmail.com", "userPassword": "Password@123"})
     response_json = response.json()
+
+def test_view_order(playwright:Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com/client")
+    page.get_by_role("textbox", name="email@example.com").fill("prakhar.sh95@gmail.com")
+    page.get_by_role("textbox", name="enter your passsword").fill("Password@123")
+    page.get_by_role("button", name="Login").click()
+    page.get_by_role("button", name="ORDERS").click()
+    page.get_by_role("button", name="View").first.click()
+    page.get_by_role("button", name="Sign Out").click()
+
+    context.close()
+    browser.close()
+
